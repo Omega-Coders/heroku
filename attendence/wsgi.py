6879d03a23092b5535cea5b53e269c46330c2029
manikanta700 <20pa1a5455@vishnu.edu.in>
2022-04-12 16:11:03 +0530

    ver--

--- attendence/wsgi.py
+++ attendence/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendence.settings')
 
 application = get_wsgi_application()
+
+import sys
+sys.path.append("C:UsersmanikQR_project/combine_qr/QRProject1/QRProject1/attendence")
